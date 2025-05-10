1acac5a7c34acc3dc377aef2f46e6f2645f1630f
MatiasMaccaroni <mmaccaroni@frba.utn.edu.ar>
2025-05-09 21:35:34 -0300

    Primera version

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,7 @@
 module Library where
 import PdePreludat
+import GHC.IO.Windows.Handle (HANDLE)
+import GHC.Generics (prec)
 
 data Ingrediente =
     Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras
@@ -17,4 +19,34 @@ data Hamburguesa = Hamburguesa {
     precioBase :: Number,
     ingredientes :: [Ingrediente]
 } deriving (Eq, Show)
+
+
+agrandar :: Hamburguesa -> Hamburguesa
+agrandar hamburguesa
+    | Carne `elem` ingredientes hamburguesa = hamburguesa {precioBase = precioBase hamburguesa + 20, ingredientes = Carne : ingredientes hamburguesa }
+    | Pollo `elem` ingredientes hamburguesa = hamburguesa {precioBase = precioBase hamburguesa + 10, ingredientes = Pollo : ingredientes hamburguesa}
+    | otherwise = hamburguesa { precioBase = precioBase hamburguesa + 20, ingredientes = Carne : ingredientes hamburguesa }
+
+agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
+agregarIngrediente ingrediente hamb =
+    hamb {precioBase = precioBase hamb + precioIngrediente ingrediente, ingredientes = ingrediente : ingredientes hamb}
+
+descuento :: Number -> Hamburguesa -> Hamburguesa
+descuento des hamb = hamb{precioBase = precioBase hamb - (precioBase hamb * des / 100)}
+
+cuartoDeLibra :: Hamburguesa
+cuartoDeLibra = Hamburguesa {
+    precioBase = 20,
+    ingredientes = [Pan, Carne, Cheddar, Pan]
+}
+
+pdepBurguer :: Hamburguesa
+pdepBurguer = descuento 20 . agregarIngrediente Cheddar . agregarIngrediente Panceta . agrandar . agrandar $ cuartoDeLibra
+
+-- dobleCuarto = es un cuarto de libra con carne y cheddar. El precio final deberia ser 84.
+
+dobleCuarto :: Hamburguesa
+dobleCuarto = agregarIngrediente Cheddar . agrandar $ cuartoDeLibra
+
+
 
